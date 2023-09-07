691b7a01308bf3b133933e3a3932539d7381f7f4
SwagataMOfficial <swagatamukherjee365@gmail.com>
2023-09-07 21:17:05 +0530

    updated home file

---
+++ logout.php
@@ -0,0 +1,9 @@
+<?php
+    echo '<script>
+    alert("Logged out successfully");
+    window.location.replace("student_home.php");
+</script>';
+
+// header("Location: student_home.php");
+// header("Location: main.php");
+?>
