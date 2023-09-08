39a24445604099fb3fb8217acfdeed2bafe65d51
SwagataMOfficial <swagatamukherjee365@gmail.com>
2023-09-08 20:04:02 +0530

    changed title of every page

--- readMessage.php
+++ readMessage.php
@@ -45,7 +45,7 @@ if (isset($_POST["createAccount"]) && $_POST["createAccount"] == "createAccount"
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>Read Messages</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
 </head>
--- student_home.php
+++ student_home.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP and HTML</title>
+    <title>Home Page</title>
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
     <link rel="stylesheet" href="css/index.css">
