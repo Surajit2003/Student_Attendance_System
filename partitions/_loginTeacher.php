96688a0c6c1e4be5f2647b90538acca728fcd284
SwagataMOfficial <swagatamukherjee365@gmail.com>
2023-09-14 21:38:24 +0530

    changed ids and labels

--- partitions/_loginTeacher.php
+++ partitions/_loginTeacher.php
@@ -10,16 +10,15 @@
                 <!-- create account form -->
                 <form action="loginTeacher.php" method="post" id="loginTeacher">
                     <div class="mb-3">
-                        <label for="exampleInputEmail1" class="form-label">Email</label>
-                        <input type="email" name="email" class="form-control" id="exampleInputEmail1"
+                        <label for="email" class="form-label">Email</label>
+                        <input type="email" name="email" class="form-control" id="email"
                             aria-describedby="emailHelp" placeholder="Enter Your Email" required>
                     </div>
                     <div class="mb-3">
-                        <label for="exampleInputEmail1" class="form-label">Password</label>
-                        <input type="password" name="password" class="form-control" id="exampleInputEmail1"
+                        <label for="password" class="form-label">Password</label>
+                        <input type="password" name="password" class="form-control" id="password"
                             aria-describedby="emailHelp" placeholder="Enter Your Password" required>
                     </div>
-
                 </form>
             </div>
             <div class="modal-footer">
